76dc0d03850673ec65909d6ae4ed9745938264aa
Anoop Varghese <anoop@metaboard.io>
2016-05-17 00:50:46 +0800

    sample tests

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 import express from 'express';
-
+import bodyParser from 'body-parser';
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.json({ hello: 'All the best' });
 });
 
+app.post('/', (req, res) => {
+  const data = req.body;
+  const filteredData = typeof data.payload !== 'undefined' ?
+              data.payload.filter(m => (m.drm === true && m.episodeCount > 0)) :
+              null;
+  if (!filteredData) {
+    return res.status(400).send({ error: 'Could not decode request: JSON parsing failed' });
+  }
+  const respData = filteredData.map(m => {
+    const obj = {};
+    obj.image = m.image.showImage;
+    obj.slug = m.slug;
+    obj.title = m.title;
+    return obj;
+  });
+  if (respData.length === 0) {
+    return res.status(400).send({ error: 'Could not decode request: JSON parsing failed' });
+  }
+  return res.json(respData);
+});
 
 const port = process.env.PORT || 3000;
-
 app.listen(port, () => {
   console.log(`Service started on port : ${port}`);
 });
--- test/index.js
+++ test/index.js
@@ -1,12 +1,58 @@
-import http from 'http';
+import request from 'superagent';
 import assert from 'assert';
 import { describe, it } from 'mocha';
 import '../index.js';
 
-describe('Example node server', () => {
+const req1 = require('./test_sample/sampleReq1.json');
+const resp1 = require('./test_sample/sampleResp1.json');
+const req2 = require('./test_sample/sampleReq2.json');
+const req3 = require('./test_sample/sampleReq3.json');
+const errorResp = require('./test_sample/errorResp.json');
+
+describe('server', () => {
   it('should return 200', done => {
-    http.get('http://localhost:3000', res => {
-      assert.equal(200, res.statusCode);
+    request.get('localhost:3000').end((err, res) => {
+      assert.equal(200, res.status);
+      assert.deepEqual(JSON.parse('{"hello": "All the best" }'), JSON.parse(res.text));
+      done();
+    });
+  });
+});
+
+describe('Example submit valid data', () => {
+  it('should post data', done => {
+    request.post('localhost:3000')
+    .set('Content-Type', 'application/json')
+    .send(JSON.stringify(req1))
+    .end((err, res) => {
+      assert.equal(200, res.status);
+      assert.equal(JSON.stringify(resp1), res.text);
+      done();
+    });
+  });
+});
+
+describe('Example submit invalid data', () => {
+  it('should not post data', done => {
+    request.post('localhost:3000')
+    .set('Content-Type', 'application/json')
+    .send(JSON.stringify(req2))
+    .end((err, res) => {
+      assert.equal(400, res.status);
+      assert.equal(JSON.stringify(errorResp), res.text);
+      done();
+    });
+  });
+});
+
+describe('Example submit invalid data 2', () => {
+  it('should not post data', done => {
+    request.post('localhost:3000')
+    .set('Content-Type', 'application/json')
+    .send(JSON.stringify(req3))
+    .end((err, res) => {
+      assert.equal(400, res.status);
+      assert.equal(JSON.stringify(errorResp), res.text);
       done();
     });
   });
